f0351f6e2c54f3b1b6d0cc551efe215ee9a76be7
ronanl <skimpydoo@gmail.com>
2012-06-27 18:38:44 +0100

    updated files

---
+++ EFMotorist/PenaltyPointsMVC3/PenaltyPointsMVC3/PenaltyPointsModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\AnthonyOToole\Desktop\Semester 3\EntityFrameworkResearch\4.Project Versions\EFMotorist\PenaltyPointsMVC3\PenaltyPointsMVC3\PenaltyPointsModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
